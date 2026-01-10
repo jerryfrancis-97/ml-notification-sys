708eefa3d16cd63431132603f2bac512fe32dc0e
Jerome Francis <francis.jerome999@gmail.com>
2026-01-10 18:50:19 -0500

    update simulator, add logging feat

--- generate_logs.py
+++ generate_logs.py
@@ -5,12 +5,13 @@ import os
 import time
 
 
-def ControlPolicy(user_data):
+class ControlPolicy(): 
     """
     Controls the policy for the users
     """
     
-    def fixed_policy(self):
+    @staticmethod
+    def fixed_policy():
         """ give out notification at 9am always """
         return 9
 
@@ -19,17 +20,17 @@ class LogGenerator:
     Generates send and response logs for the users
     """
 
-    def __init__(self, user_data):
-
-        self.user_data = pd.read_csv(user_data)
+    def __init__(self, user_data_path, logs_path):
+        self.logs_path = logs_path
+        self.user_data = pd.read_csv(user_data_path)
         
         send_logs = []
         response_logs = []
 
         for day in range(30):  # 30 days
             for index, row in self.user_data.iterrows():
-                hour = ControlPolicy.fixed_policy(user_id)
-                user_id = row["user_id"]
+                hour = ControlPolicy.fixed_policy()
+                user_id = row["name"]
                 peak_hour = row["peak_hour"]
                 peak_probability = row["peak_probability"]
                 user_type = row["user_type"]
@@ -52,4 +53,20 @@ class LogGenerator:
                         "event_id": event_id,
                         "opened": int(opened)
                     })
-            return send_logs, response_logs
+        
+        self.send_logs = pd.DataFrame(send_logs)
+        self.response_logs = pd.DataFrame(response_logs)
+
+        self.save_logs()
+
+    def save_logs(self):
+        """ save the logs to a csv file """
+        self.send_logs.to_csv(os.path.join(self.logs_path, "send_logs.csv"), index=False)
+        self.response_logs.to_csv(os.path.join(self.logs_path, "response_logs.csv"), index=False)
+    
+
+if __name__ == "__main__":
+    user_data_path = "data/user_data.csv"
+    logs_path = "data/logs"
+    os.makedirs(logs_path, exist_ok=True)
+    log_generator = LogGenerator(user_data_path, logs_path)
