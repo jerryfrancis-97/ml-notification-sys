987604dde2e51aa000ef96639ab98e01c5d10d22
Jerome Francis <francis.jerome999@gmail.com>
2026-01-22 17:45:41 -0500

    sample dvc commit sh after new feature add

--- feature_engg_tracking.sh
+++ feature_engg_tracking.sh
@@ -2,7 +2,7 @@
 
 # 1. Modify feature_engg.py with new features
 # 2. Re-run feature engineering
-python feature_engg.py
+# python feature_engg.py
 
 # 3. DVC automatically detects changes
 dvc status  # Shows modified files
@@ -12,8 +12,8 @@ dvc add data/training_data_features.csv
 
 # 5. Commit the new version
 git add data/training_data_features.csv.dvc
-git commit -m "feat: add interaction features + drop hour column"
-git tag -a "features-v1.1" -m "Added interaction features + dropped hour column"
+git commit -m "feat: add time bucket open rates"
+git tag -a "features-v1.3" -m "Added time bucket open rates"
 
 # 6. Push
 dvc push
